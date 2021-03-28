a731e242ee4269dcec891667ac1a6b66ee2ea1d9
Aaron D Medero <47156323+AssumetheWorst@users.noreply.github.com>
2021-03-28 11:08:57 -0400

    First commit

--- src/components/Table.js
+++ src/components/Table.js
@@ -13,13 +13,15 @@ const TableHeader = () => {
     );
 }
 
+// const linkData = [];
+
 const TableBody = props => { 
     // boilerplate table body functional component 
     // we use Array.map to create table rows from LinkData passed via props
     const rows = props.linkData.map((row, index) => {
         return (
             <tr key={index}>
-                <td>{row.name}</td>
+                <td>{row.Name}</td>
                 <td><a href={row.URL}>{row.URL}</a></td>
                 <td><button onClick={() => props.removeLink(index)}>Delete</button></td>
             </tr>
@@ -30,9 +32,41 @@ const TableBody = props => {
 }
 
 const Table = (props) => {
-//    const { linkData, removeLink } = props;
-    {/*TODO - return <table> component, TableHeader and TableBody  */}
+    const { linkData, removeLink } = props;
+     //add <TableBody /> to the bottom of <TableHeader>
+ 
+     return (
+         <div>
+             <table>
+                 <TableHeader />
+                 <TableBody linkData = {linkData} removeLink = {removeLink}/>
+             </table>
+         </div>
+ 
+     )
+ 
+ }
+ 
+ export default Table;
 
-}
 
-export default Table;
+/* Code for making table from W3 schools
+[<table> 
+                <tr>
+                <th>Name</th>
+                <th>URL</th>
+                <th>Remove</th>
+                </tr>
+                <tr>
+                <td>Jill</td>
+                <td>Smith</td>
+                <td>50</td>
+                </tr>
+                <tr>
+                <td>Eve</td>
+                <td>Jackson</td>
+                <td>94</td>
+                </tr>
+            </table>]
+
+        */
